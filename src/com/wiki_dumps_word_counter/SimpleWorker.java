5c0b7983f9e9ce7126e0eaa050eb76715ccd425a
Evyatar Meged <cambina1@gmail.com>
2017-08-27 23:52:56 +0300

    Add method to worker- using regex to eliminate any non-hebrew chars. pool now terminates only when article queue is empty. method refactoring

--- src/com/wiki_dumps_word_counter/SimpleWorker.java
+++ src/com/wiki_dumps_word_counter/SimpleWorker.java
@@ -2,32 +2,45 @@ package com.wiki_dumps_word_counter;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class SimpleWorker implements Runnable {
+    // TODO: Implement a method for stripping connection words / plural
 
     private static final String CHARSET = "אבגדהוזחטיכלמנסעפצקרשת";
+    private static final Pattern PATTERN = Pattern.compile("[^"+CHARSET+"]");
     private StringBuilder data;
     private Stream<String> lines;
+    private ArticleQueue articleQueue = ArticleQueue.getInstance();
+    private FileNameQueue fileNameQueue = FileNameQueue.getInstance();
 
 
-    private void ParseFileContents() throws IOException {
-        // Read file contents, remove unwanted chars and push to ParsedArticleQueue
-        while (!FileNameQueue.isEmpty()) {
-            data = new StringBuilder();
-            lines = Files.lines(FileNameQueue.pop());
-            lines.forEach(line -> data.append(line).append("\n"));
-            lines.close();
-        }
-//         TODO: implement better text parsing (remove all chars except א-ב)
-        ParsedArticleQueue.insert(data.toString().replaceAll("[\\w]", " "));
+    private String stripNonHebrewChars(String text) {
+        // Strip all non hebrew characters
+        Matcher matcher = PATTERN.matcher(text);
+        text = matcher.replaceAll(" ");
+        return text.trim();
     }
 
+    private void EnqueueParsedArticle() throws IOException {
+        // Read file contents, remove unwanted chars and push to ArticleQueue
+        while (!this.fileNameQueue.isEmpty()) {
+            data = new StringBuilder();
+            lines = Files.lines(this.fileNameQueue.pop());
+            lines.forEach(line -> data.append(line).append("\n"));
+            lines.close();
+            this.articleQueue.insert(this.stripNonHebrewChars(data.toString()));
+        }
+    }
+
+    @Override
     public void run() {
         try {
-            this.ParseFileContents();
+            this.EnqueueParsedArticle();
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(System.err);
         }
     }
 }
--- src/com/wiki_dumps_word_counter/WorkersPool.java
+++ src/com/wiki_dumps_word_counter/WorkersPool.java
@@ -5,17 +5,20 @@ import java.util.concurrent.Executors;
 
 
 public class WorkersPool {
+
+    private ArticleQueue articleQueue = ArticleQueue.getInstance();
+
     private static final int CORES = Runtime.getRuntime().availableProcessors();
 
-    public static void main(String[] args) {
+    void execute () {
+        // Tried with 4-8 cores, result is always around 35-40 seconds E2E
         ExecutorService pool = Executors.newFixedThreadPool(CORES);
         for (int i = 0; i < CORES; i++) {
             Runnable worker = new SimpleWorker();
             pool.execute(worker);
         }
+
+        while (!this.articleQueue.isEmpty()) {}
         pool.shutdown();
-        if (pool.isTerminated()) {
-            System.out.println("All workers are terminated");
-        }
     }
 }
